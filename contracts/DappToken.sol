58fdebab06b1156e89f30c994b6987c1d1d6f1bd
Akanksha740 <akanksha.1999.sharma@gmail.com>
2022-06-11 23:45:57 +0530

    first commit

---
+++ contracts/DappToken.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.5.16;
+
+
+
+contract DappToken{ 
+    //Name
+    string public name= "DApp Token";
+    string public symbol= "DAPP";
+    string public standard= "DApp Token v1.0"; //SAME SAME AS IN TEST
+    //Symbol
+    //Constructor
+    //Set the total number of tokens
+    //Read the total number of tokens  
+    
+    uint256 public totalSupply;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+       uint256 _value 
+    );
+    //approve
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+    mapping(address => uint256) public balanceOf;//who has the tokens
+    mapping(address => mapping(address => uint256))public allowance;
+    
+    //Allowance
+    constructor(uint256 _initialSupply) public{
+        balanceOf[msg.sender]=_initialSupply;
+        totalSupply= _initialSupply;
+        //allocate the initital  supply
+
+    }
+    //Transfer
+    function transfer(address _to,uint256 _value)public returns (bool success) {
+        //Exception if account doesnt have enough
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+    
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+        //Transfer the balance
+        //Return a boolean
+        //Transfer Event
+    }
+    //Approve
+    function approve(address _spender,uint256 _value)public returns (bool success){
+        //allowance
+        allowance[msg.sender][_spender]=_value;
+
+        // approve event
+        emit Approval(msg.sender, _spender,_value);
+        return true;
+    }    //TransferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+         //require _from has enough tokens
+        require(_value<=balanceOf[_from]);
+        //require allowance is big enough
+        require(_value<=allowance[_from][msg.sender]);
+        //change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //update the allowance
+        allowance[_from][msg.sender] -= _value;
+        //transfer event
+        emit Transfer(_from,_to,_value); 
+        //return a boolean
+        return true;       
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,19 @@
+const DappToken = artifacts.require("DappToken");
+const DappTokenSale = artifacts.require("DappTokenSale");
+module.exports = function (deployer) {
+    deployer.deploy(DappToken, 1000000).then(function () {
+        var tokenPrice = 1000000000000000;
+        return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+    });
+};
+
+// var DappToken = artifacts.require("./DappToken.sol");
+// var DappTokenSale = artifacts.require("./DappTokenSale.sol");
+
+// module.exports = function (deployer) {
+//     deployer.deploy(DappToken, 1000000).then(function () {
+//         // Token price is 0.001 Ether
+//         var tokenPrice = 1000000000000000;
+//         return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+//     });
+// };
